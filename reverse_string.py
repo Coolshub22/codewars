491f004fb6f4f8ea0e94089c35c40f360d806e88
Arnold <arnoldkulavi@gmail.com>
2025-05-18 11:33:25 +0300

    feat:reverse sentence

--- reverse_string.py
+++ reverse_string.py
@@ -4,3 +4,15 @@ def solution(string):
 print(solution("cools"))
 
 # to reverse a string we use reverse() or [::-1]
+
+
+# REVERSE EXAMPLE 2:
+def spin_words(sentence):
+# break the sentence into a list of words using split()
+    words = sentence.split()     
+# list comprehension
+# reverse then join
+    r_sentence = [word[::-1] if len(word) >= 5 else word
+                  for word in words]
+    return ' '.join(r_sentence)
+print(spin_words("I We think about ourselves always"))
